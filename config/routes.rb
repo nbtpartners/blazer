7324202f67c8c5ecc71322cace15bbde75513635
kang.hojung <kang.hojung@nbt.com>
2018-02-05 17:33:42 +0900

    추가: devise gem을 활용하여 blazer dashboard에  접속시 로그인을 해야만 접속이 가능하도록 수정하다.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'welcome#index'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  mount Blazer::Engine, at: 'blazer'
+  #TODO user.present? 를 user.admin? 으로 교체한다
+  authenticate :user, -> (user) { user.present? } do
+    mount Blazer::Engine, at: "blazer"
+  end
 end
